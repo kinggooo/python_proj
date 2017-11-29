22e035df33c5e7cf482e89e382d8a918eb7a429a
wangnz <5323700@qq.com>
2017-11-29 09:59:23 +0800

    add

--- test_1/test1.py
+++ test_1/test1.py
@@ -1,1 +1,14 @@
-print('hello')
+msg="hello python world!"
+print(msg.title())
+list1=['a','b','c']
+print(list1)
+list1.reverse()
+print(list1)
+for x in list1:
+    print(x)
+
+for value in range(1,10):
+    print(value)
+
+ls=list(range(10,20))
+print(ls)
